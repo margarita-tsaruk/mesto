0929c52211caf5ba09ac50d95bc9eccad7ece0d6
Maggie <margarital@MacBook-Air-Margarita.local>
2022-03-28 22:38:40 +0200

    2803

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,23 @@ editProfile.addEventListener('click', openEditForm);
 modalCloseBtn.addEventListener('click', function () {
     modalWindow.classList.remove('popup_opened');
 });
+
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.input__text_type_name');
+let jobInput = document.querySelector('.input__text_type_job');
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let profileName = document.querySelector('.profile__name');
+   profileName.textContent = nameInput.value;
+   let profileJob = document.querySelector('.profile__job');
+   profileJob.textContent = jobInput.value;
+   modalWindow.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
