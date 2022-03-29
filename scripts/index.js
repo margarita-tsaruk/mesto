eb3320b12b6c2e3332ba493c135b858d964bae47
Maggie <margarital@MacBook-Air-Margarita.local>
2022-03-29 22:36:08 +0200

    feat: Complete work on project 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,29 @@
-const editProfile = document.querySelector('.profile__edit-button');
-const modalWindow = document.querySelector('.popup');
-const modalCloseBtn = modalWindow.querySelector('.popup__close-button');
-
-function openEditForm(){
-    modalWindow.classList.add('popup_opened');
-}
-editProfile.addEventListener('click', openEditForm);
-
-modalCloseBtn.addEventListener('click', function () {
-    modalWindow.classList.remove('popup_opened');
-});
-
-
+let editProfile = document.querySelector('.profile__edit-button');
+let modalWindow = document.querySelector('.popup');
+let modalCloseBtn = modalWindow.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.input__text_type_name');
-let jobInput = document.querySelector('.input__text_type_job');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
+function openEditForm() {
+  modalWindow.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function closeEditForm() {
+  modalWindow.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let profileName = document.querySelector('.profile__name');
-   profileName.textContent = nameInput.value;
-   let profileJob = document.querySelector('.profile__job');
-   profileJob.textContent = jobInput.value;
-   modalWindow.classList.remove('popup_opened');
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closeEditForm();
 }
 
+editProfile.addEventListener('click', openEditForm);
+modalCloseBtn.addEventListener('click', closeEditForm);
 formElement.addEventListener('submit', formSubmitHandler);
-
-
-
