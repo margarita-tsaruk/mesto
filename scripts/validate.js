0e46bfaa4409e1d8932a30c6a266998d674f8bb8
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-04-24 22:06:45 +0200

    feat: working on 1 popup

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,29 +1,56 @@
 
 
-function enableValidation(formSelector) {
-  const forms = document.querySelectorAll(formSelector);
-  forms.forEach((element) => {
-     element.addEventListener('submit', (event) => handleFormSubmit(event, element));
-     element.addEventListener('input', (event) => handleFormInput(event, element));
-  })
+function enableValidation(config) {
+  const form = document.querySelector(config.formSelector);
+  const inputs = form.querySelectorAll(config.inputSelector);
+
+//  forms.forEach((form) => {
+form.addEventListener('submit', (event) => handleFormSubmit(event, form));
+//  })
+
+  inputs.forEach((element) => {
+    element.addEventListener('input', (event) => handleFormInput(event, form, config));
+ })
+
+  toggleButton(form, config);
 }
+
+function toggleButton(form, config) {
+  const button = document.querySelector(config.submitButtonSelector);
+  button.disabled = !form.checkValidity();
+  button.classList.toggle('popup__button_disabled', !form.checkValidity());
+}
+
 /**
  * Обработать событие валидации формы
  * @param {SubmitEvent} event
- * @param {HTMLFormElement} element
+ * @param {HTMLFormform} form
  */
-function handleFormSubmit(event, element) {
+function handleFormSubmit(event, form) {
   event.preventDefault();
 
-    if (element.checkValidity()) {
+    if (form.checkValidity()) {
     alert('Form is valid');
   } else {
     alert('Not valid');
   }
 }
 
-function handleFormInput(event) {
+function handleFormInput(event, form, config) {
   const input = event.target;
+  const error = document.querySelector(`#${input.id}-error`);
+
+  if (input.validity.valid) {
+    error.textContent = '';
+  } else {
+   error.textContent = input.validationMessage;
+  }
+
+  toggleButton(form, config);
 }
 
-enableValidation('.popup__form');
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+});
