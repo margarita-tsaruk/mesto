93cffb5c3f9ea291b84226ad9a441a612a1ac545
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-04-26 13:19:57 +0200

    fix: 2604 - 1

--- scripts/validate.js
+++ scripts/validate.js
@@ -13,65 +13,64 @@ function enableValidation() {
 
     setEventListeners(formElement);
   });
-
 }
 
 enableValidation();
 
 /**
  * Добавить слушатель событий всем полям ввода внутри формы
- * @param {HTMLFormform} inputs
+ * @param {HTMLFormform} inputList
  */
 function setEventListeners(formElement) {
-  const inputs = Array.from(formElement.querySelectorAll('.popup__input'));
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
   const buttonElement = formElement.querySelector('.popup__button');
 
-  toggleButton(inputs, buttonElement);
-
-  inputs.forEach((inputElement) => {
+  inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       checkInputValidity(formElement, inputElement);
-      toggleButton(inputs, buttonElement);
+      toggleButton(inputList, buttonElement);
     });
  });
 }
 
-//Проверить полей ввода на валидность
-const checkInputValidity = (formElement, inputElement) => {
-  if(!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-}
-
 //Проверить невалидность полей ввода (применить функцию если хотя бы 1 поле невалидно)
-function hasInvalidInput(inputs) {
-  return inputs.some((inputElement) => {
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   })
 }
 
 //Включить и отключить состояние кнопки при проверки валидности полей формы
-const toggleButton = (inputs, buttonElement) => {
-  if (hasInvalidInput(inputs)) {
+function toggleButton(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
     buttonElement.classList.add('popup__button_disabled');
+    buttonElement.setAttribute('disabled', 'disabled');
   } else {
     buttonElement.classList.remove('popup__button_disabled');
+    buttonElement.removeAttribute('disabled', 'disabled');
   }
-};
+}
 
+//Проверить полей ввода на валидность
+function checkInputValidity(formElement, inputElement) {
+  if(!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+  toggleButton(inputList, buttonElement)
+}
 
 //Показать сообщение об ошибке в полях ввода
-const showInputError = (formElement, inputElement, errorMessage) => {
+function showInputError(formElement, inputElement, errorMessage) {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
-  inputElement.classList.add('popup__input_type_invalid');
   errorElement.textContent = errorMessage;
+  inputElement.classList.add('popup__input_type_invalid');
 }
 
 //Скрыть сообщение об ошибке в полях ввода
-const hideInputError = (formElement, inputElement) => {
+function hideInputError(formElement, inputElement) {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
   inputElement.classList.remove('popup__input_type_invalid');
