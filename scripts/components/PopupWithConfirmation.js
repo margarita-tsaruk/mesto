2df1ae2663f87423bbc4d7c2e4c66b42d9accfb5
margarita-tsaruk <margarita.tsaruk@yandex.ru>
2022-06-15 21:44:16 +0200

    1506: 4

--- scripts/components/PopupWithConfirmation.js
+++ scripts/components/PopupWithConfirmation.js
@@ -3,9 +3,8 @@ export default class PopupWithConfirmation extends Popup {
   constructor ( popupSelector ) {
     super(popupSelector);
     this._form  = this._popup.querySelector('.popup__form');
+   console.log(this._form)
   }
-
-
 
   //Объявление публичного метода: добавить слушателей событий
   setEventListeners() {
